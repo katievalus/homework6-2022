c055d81485ede8a8c5604eebb70a997daf411330
katievalus <97191659+katievalus@users.noreply.github.com>
2022-04-13 17:34:36 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -62,6 +62,14 @@ document.querySelector("#slider").addEventListener("click", function() {
 	
 });
 
+document.querySelector('#vintage').addEventListener('click', function(){
+	video.classList.add("oldSchool")
+})
+
+document.querySelector('#orig').addEventListener('click', function(){
+	video.classList.remove("oldSchool")
+})
+
 
 
 
